955444074ed1e8d10855b911e8e181ecd9dd465f
LucianoIvanBlanco <luciano.blanco@live.u-tad.com>
2024-04-09 16:44:13 +0200

    server ok

---
+++ config/plugins.js
@@ -0,0 +1,59 @@
+const fechaActual = new Date();
+const añoActual = fechaActual.getFullYear();
+const mesActual = fechaActual.getMonth() + 1;
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('https://prueba-tecnica-strapi.s3.eu-west-1.amazonaws.com/'),
+        rootPath: `${env('CDN_ROOT_PATH')}${añoActual}/${mesActual}`,
+        s3Options: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('AWS_BUCKET')
+          }
+        }
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {}
+      }
+    }
+  },
+  'import-export-entries': {
+    enabled: true,
+    config: {}
+  },
+  
+  'transformer': {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true
+      }
+    }
+  },
+  translate: {
+    enabled: true,
+    config: {
+      provider: 'deepl',
+      providerOptions: {
+        apiKey: '4c58f016-006c-3951-31eb-dd72860753b1:fx',
+        localeMap: {
+          ES: 'ES'
+        },
+        apiOptions: {
+          formality: 'default'
+        }
+      }
+    }
+  }
+});
